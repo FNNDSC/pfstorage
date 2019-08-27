2584120289e64b2771ad3637e079ff6b0d8408a2
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2019-08-27 17:42:57 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 
 setup(
       name             =   'pfstorage',
-      version          =   '1.1.0.0',
+      version          =   '1.1.0.2',
       description      =   'object storage interface',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
